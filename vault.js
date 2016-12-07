54840af3b94c2780fde82f5c8038bdba2777a2ae
jhoun <jayhoun@gmail.com>
2016-12-06 19:28:39 -1000

    Test passes: vault protected

--- vault.js
+++ vault.js
@@ -1,4 +1,26 @@
 'use strict';
 module.exports = function() {
-  
+
+  //Private variables
+  var jaysVault ={};
+
+  //private methods
+  function setValue(prop, val){
+    jaysVault[prop] = val;
+  }
+
+  function getValue(key){
+    if(jaysVault[key]){
+      return jaysVault[key];
+    }
+    else {
+      return null;
+    }
+  }
+
+  return {
+    getValue: getValue,
+    setValue: setValue
+  }
+
 };
